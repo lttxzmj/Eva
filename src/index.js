6272ad3d5a9c205c23cf7d138ff894cd33c6b73b
Shoucheng H <h.shoucheng@gmail.com>
2017-05-10 16:02:56 +0800

    Implement a prototype of header component
    With normalize.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HomePage from './components/pages/home/index.jsx';
+import './style/index.sass';
 
-function App() {
-    return <div > hello, world < /div>;
+function App (){
+  return(
+    <div>
+      <HomePage />
+    </div>
+  )
 }
 
 const root = document.getElementById('root');
-ReactDOM.render( < App / > , root);
+ReactDOM.render(
+   <App /> ,
+    root
+  )
+
+
+//@ sourceMappingURL=/path/to/file.js.map
