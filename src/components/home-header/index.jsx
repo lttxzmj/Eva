b12f18c9cefdd4d466c101df994115130c79970a
Shoucheng H <h.shoucheng@gmail.com>
2017-05-10 17:35:10 +0800

    style of header.

--- src/components/home-header/index.jsx
+++ src/components/home-header/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function HomeHeader(props){
   return (
     <div>
-      <div className="header-search">
+      <div className="header-search center">
         <div className="search">
         <i className="search__icon"></i>
         <input type="text" className="search__input" placeholder="Search for a product" />
