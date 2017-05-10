2cabf4d0250b0df89b6c6b69ddb4ecb7822cc6cd
Shoucheng H <h.shoucheng@gmail.com>
2017-05-10 16:01:00 +0800

    Update toolchain: sass and fontawesome

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,64 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle-[hash].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            include: [
-                path.resolve(__dirname, 'src'),
-            ],
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle-[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+            path.resolve(__dirname, 'src'),
+        ],
+        use: [
+          {
             loader: 'babel-loader',
             options: {
-                presets: ['es2015', 'es2016', 'es2017', 'react'],
+              presets: ['es2015', 'es2016', 'es2017', 'react'],
             },
-        }, ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html',
-            inject: 'body',
+          }
+        ],
+      },
+      {
+        test: /\.s[ca]ss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader?sourceMap',
+            'sass-loader?sourceMap',
+          ],
         }),
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader?sourceMap'],
+        }),
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)(\?.*$|$)/,
+        use: ['file-loader'],
+      },
     ],
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        historyApiFallback: true,
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new ExtractTextPlugin('bundle-[hash].css'),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+    overlay: true,
+  },
+  devtool: "source-map"
 };
