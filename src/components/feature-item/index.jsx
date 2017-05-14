7f7afbc15c12904b2aef4e78e2a78977803782b7
Shoucheng H <h.shoucheng@gmail.com>
2017-05-14 14:17:56 +0800

    finish the structure of the  feature today and feature item.

--- src/components/feature-item/index.jsx
+++ src/components/feature-item/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class FeatureItem extends React.Component{
   render() {
@@ -6,20 +7,29 @@ class FeatureItem extends React.Component{
       <div>
         <div className="item">
           <div className="item__photo">
-            <img src={source}/>
+            <img src={this.props.item.imageUrl}/>
           </div>
           <div className="item__describe">
             <div className="item__name">
-              {this.props.name}
+              {this.props.item.name}
             </div>
             <div className="item__info">
-              {this.props.info}
+              {this.props.item.info}
             </div>
           </div>
         </div>
       </div>
     );
   }
+};
+
+FeatureItem.propTypes = {
+  item: PropTypes.shape({
+    name: PropTypes.string,
+    info: PropTypes.string,
+    imageUrl: PropTypes.string,
+    fontSize: PropTypes.number
+  }),
 };
 
 export default FeatureItem;
--- src/components/featured-today/index.jsx
+++ src/components/featured-today/index.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import FeatureItem from '../feature-item/index.jsx';
-import item1 from './img/1/JPG';
 
 export default function FeaturedToday(props){
-  return(
-    <div>
-      <div className="feature">
-        <div className="feature__title">
-          <div className="feature__goods">
-            <FeatureItem name="Cosmo Black" info="Modern black armchair"/>
-            <FeatureItem name="Phux" info="Modern pink armchair"/>
-            <FeatureItem />
-            <FeatureItem />
+  const itemList = [
+    {
+      "name": "Cosmo Black",
+      "info": "Modern black armchair",
+      "imageUrl": "https://d3dr1ze7164817.cloudfront.net/items/2A3d3y0G3E3N3Z3X1M3i/1.JPG?X-CloudApp-Visitor-Id=2756471",
+    },
+    {
+      "name": "Cosmo Black",
+      "info": "Modern black armchair",
+      "imageUrl": "https://d3dr1ze7164817.cloudfront.net/items/2h3k3t071O0y0F0P1b3L/2.JPG?X-CloudApp-Visitor-Id=2756471",
+    },
+    {
+      "name": "Cosmo Black",
+      "info": "Modern black armchair",
+      "imageUrl": "https://d3dr1ze7164817.cloudfront.net/items/0x3v1m341f220G1R1u3K/3.JPG?X-CloudApp-Visitor-Id=2756471",
+    },
+    {
+      "name": "Cosmo Black",
+      "info": "Modern black armchair",
+      "imageUrl": "https://d3dr1ze7164817.cloudfront.net/items/2h3k3t071O0y0F0P1b3L/2.JPG?X-CloudApp-Visitor-Id=2756471",
+    }
+  ];
+
+    return(
+      <div>
+        <div className="feature">
+          <div className="feature__title">
+            <div className="feature__goods">
+              {itemList.map((item,index) =>
+                <FeatureItem key={index} item={item}/>
+            )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+
 }
